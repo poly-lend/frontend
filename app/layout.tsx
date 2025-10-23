04df4a1d948c2d9c7f2d18822a4720640c547dcf
polylend-dev <polylend@tuta.com>
2025-10-23 11:28:00 +0400

    ui improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Nav from "@/components/nav";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Nav />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
