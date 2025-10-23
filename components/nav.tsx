a4db296a28f2f4e23ed1ce5e512c794e088147a4
polylend-dev <polylend@tuta.com>
2025-10-23 11:28:14 +0400

    ui improvements

---
+++ components/nav.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Link from "next/link";
+
+export default function Nav() {
+  const link = { marginRight: 12 };
+  return (
+    <nav style={{ padding: 16, borderBottom: "1px solid #eee" }}>
+      <Link href="/" style={link}>
+        Home
+      </Link>
+      <Link href="/lend" style={link}>
+        About
+      </Link>
+      <Link href="/borrow" style={link}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
