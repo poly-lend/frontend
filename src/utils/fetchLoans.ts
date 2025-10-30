27138b3d2c6ae22e3b4b7f760f259a3a1d96b22a
polylend-dev <polylend@tuta.com>
2025-10-30 15:11:43 +0400

    offers

--- src/utils/fetchLoans.ts
+++ src/utils/fetchLoans.ts
@@ -1,44 +1,49 @@
 import { polylendAddress } from "@/configs";
 import { polylendConfig } from "@/contracts/polylend";
-
-import { LoanOffer, LoanRequest } from "@/types/polyLend";
+import { Loan } from "@/types/polyLend";
 
 export const fetchOffers = async (params: {
   publicClient: any;
   address?: `0x${string}`;
-}): Promise<LoanOffer[]> => {
+}): Promise<Loan[]> => {
   const calls = [];
   for (var i = 0; i < 100; i++) {
     calls.push({
       address: polylendAddress as `0x${string}`,
       abi: polylendConfig.abi,
-      functionName: "offers",
+      functionName: "loans",
       args: [i],
     });
   }
-  const offersData = await params.publicClient.multicall({
+  const loansData = await params.publicClient.multicall({
     contracts: calls,
   });
 
-  let offers: LoanOffer[] = offersData
-    .map((offer: any, index: number) => ({
-      offerId: BigInt(index),
-      requestId: BigInt(offer.result[0]),
-      lender: offer.result[1] as `0x${string}`,
-      loanAmount: BigInt(offer.result[2]),
-      rate: BigInt(offer.result[3]),
+  let loans: Loan[] = loansData
+    .map((loan: any, index: number) => ({
+      loanId: BigInt(index),
+      borrower: loan.result[0] as `0x${string}`,
+      borrowerWallet: loan.result[1],
+      lender: loan.result[2] as `0x${string}`,
+      positionId: BigInt(loan.result[3]),
+      collateralAmount: BigInt(loan.result[4]),
+      loanAmount: BigInt(loan.result[5]),
+      rate: BigInt(loan.result[6]),
+      startTime: BigInt(loan.result[7]),
+      minimumDuration: BigInt(loan.result[8]),
+      callTime: BigInt(loan.result[9]),
     }))
 
     .filter(
-      (request: LoanRequest) =>
-        request.borrower !== `0x0000000000000000000000000000000000000000`
+      (loan: Loan) =>
+        loan.borrower !== `0x0000000000000000000000000000000000000000`
     );
   if (params.address) {
-    offers = offers.filter(
-      (request: any) =>
-        request.borrower.toLowerCase() === params.address?.toLocaleLowerCase()
+    loans = loans.filter(
+      (loan: Loan) =>
+        loan.borrower.toLowerCase() === params.address?.toLocaleLowerCase()
     );
   }
-  console.log(offers);
-  return offers;
+  console.log(loans);
+  return loans;
 };
