7cea5d770336f1ec2f62202e711cb79cb844cfc2
polylend-dev <polylend@tuta.com>
2025-10-30 15:59:50 +0400

    lending

--- src/app/lend/page.tsx
+++ src/app/lend/page.tsx
@@ -1,20 +1,9 @@
 "use client";
 
 import RequestsTable from "@/components/requestsTable";
-import { LoanRequest } from "@/types/polyLend";
-import { fetchRequests } from "@/utils/fetchRequests";
 import { Stack } from "@mui/material";
-import { useEffect, useState } from "react";
-import { usePublicClient } from "wagmi";
 
 export default function Lend() {
-  const [requests, setRequests] = useState<LoanRequest[]>([]);
-  const publicClient = usePublicClient();
-  useEffect(() => {
-    if (!publicClient) return;
-    fetchRequests({ publicClient }).then(setRequests);
-  }, [publicClient]);
-
   return (
     <Stack spacing={2}>
       <h1
