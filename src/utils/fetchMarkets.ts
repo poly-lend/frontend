67ea31aee8fae9235faed116bfe376000f27cc2b
polylend-dev <polylend@tuta.com>
2025-11-04 16:28:07 +0400

    basic hidration

--- src/utils/fetchMarkets.ts
+++ src/utils/fetchMarkets.ts
@@ -1,9 +1,17 @@
-export default async function fetchMarkets(tokenIds: string[]) {
-  const queryString = tokenIds
-    .map((tokenId) => `clob_token_ids=${tokenId}`)
-    .join("&");
-  const response = await fetch(
-    `https://gamma-api.polymarket.com/markets?${queryString}`
-  );
-  return await response.json();
+import markets from "@/data/markets.json";
+
+export default async function fetchMarkets(
+  tokenIds: string[]
+): Promise<Map<string, any>> {
+  const result = new Map<string, any>();
+  markets.forEach((market) => {
+    JSON.parse(market.clobTokenIds).forEach(
+      (tokenId: string, index: number) => {
+        const answers = JSON.parse(market.outcomes);
+        const outcome = answers[index];
+        result.set(tokenId, { market, outcome, outcomeIndex: index });
+      }
+    );
+  });
+  return result;
 }
