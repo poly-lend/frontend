4de036e4c2e8df70c88c8bd8122d61759bffd21b
polylend-dev <polylend@tuta.com>
2025-11-06 14:14:19 +0400

    Make warning more readable

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -1,7 +1,7 @@
 export default function Top() {
   return (
     <div>
-      <h1 className="text-red-500 font-bold text-center bg-yellow-500">
+      <h1 className="text-red-600 font-bold text-center bg-yellow-300 py-px">
         This is TEST page, do not use it in production, do not use real
         currency!!!
       </h1>
