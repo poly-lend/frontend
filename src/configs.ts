888873cc739133bf7bcd2dc53b79fabe6f941037
polylend-dev <polylend@tuta.com>
2025-11-11 13:07:56 +0400

    switch to latest contract

--- src/configs.ts
+++ src/configs.ts
@@ -5,6 +5,6 @@ export const polylendDecimals = 18;
 export const polymarketSharesDecimals = 6;
 
 export const proxyAddress = "0xaacFeEa03eb1561C4e67d661e40682Bd20E3541b";
-export const polylendAddress = "0x50c5ba20cef272ae1e6D2cFf1539621a2987E259";
+export const polylendAddress = "0x7bCaA23aB1777C7c19935E3872A165B10cD0F650";
 export const polymarketTokensAddress =
   "0x4D97DCd97eC945f40cF65F87097ACe5EA0476045";
