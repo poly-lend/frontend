b4d803d3df02edc58952f0f4535bd1f1ecd25328
polylend-dev <polylend@tuta.com>
2025-11-12 15:23:19 +0400

    add visual feedback for offer dialog

---
+++ src/components/widgets/loadingActionButton.tsx
@@ -0,0 +1,34 @@
+import { Box, Button, CircularProgress, type ButtonProps } from "@mui/material";
+import { ReactNode } from "react";
+
+type LoadingActionButtonProps = {
+  children: ReactNode;
+  loading?: boolean;
+} & ButtonProps;
+
+export default function LoadingActionButton({
+  children,
+  loading = false,
+  disabled,
+  ...buttonProps
+}: LoadingActionButtonProps) {
+  return (
+    <Box sx={{ m: 1, position: "relative" }}>
+      <Button disabled={loading || disabled} {...buttonProps}>
+        {children}
+      </Button>
+      {loading && (
+        <CircularProgress
+          size={24}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            marginTop: "-12px",
+            marginLeft: "-12px",
+          }}
+        />
+      )}
+    </Box>
+  );
+}
