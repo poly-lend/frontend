3182c86b4a062760807e44375113799ca8868043
polylend-dev <polylend@tuta.com>
2025-11-22 13:42:33 +0400

    add underlines

--- src/components/widgets/address.tsx
+++ src/components/widgets/address.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Address({ address }: { address: `0x${string}` }) {
   return (
     <Link href={`https://polygonscan.com/address/${address}`} target="_blank">
-      <div className="flex items-center gap-1 justify-end text-primary hover:text-primary/80 underline-offset-1=">
+      <div className="flex items-center gap-1 justify-end text-primary hover:text-primary/90 hover:underline">
         {truncateAddress(address)} <ExternalLink className="w-4 h-4" />
       </div>
     </Link>
--- src/components/widgets/market.tsx
+++ src/components/widgets/market.tsx
@@ -6,7 +6,7 @@ const Market = ({ market }: { market: any }) => {
     <Link
       href={`https://polymarket.com/event/${market.event.slug}`}
       target="_blank"
-      className="flex items-center"
+      className="flex items-center hover:underline hover:text-white/80"
     >
       <img
         width={40}
@@ -16,7 +16,7 @@ const Market = ({ market }: { market: any }) => {
         className="rounded-full mr-2"
       />
 
-      <div className="flex items-center gap-1 text-sm text-left line-clamp-2 mr-1 ">
+      <div className="flex items-center gap-1 text-sm text-left line-clamp-2 mr-1">
         {market.market.question} <ExternalLink className="w-4 h-4" />
       </div>
     </Link>
