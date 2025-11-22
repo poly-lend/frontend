0b3791c9a3720e6ff1b38d4940c9008506809767
polylend-dev <polylend@tuta.com>
2025-11-22 12:57:30 +0400

    fix svg params

--- src/components/svgs/githubIcon.tsx
+++ src/components/svgs/githubIcon.tsx
@@ -10,12 +10,12 @@ export default function DexScreenerIcon() {
         viewBox="0 -0.5 25 25"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g id="SVGRepo_bgCarrier" stroke-width="0" />
+        <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
         <g
           id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
 
         <g id="SVGRepo_iconCarrier">
