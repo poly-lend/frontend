bc7310238f5686cceb6c0e22fd19ed72de8ae4b8
polylend-dev <polylend@tuta.com>
2025-11-22 10:56:02 +0400

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
-import "@fontsource/roboto/700.css";
 import type { Metadata } from "next";
 import Children from "./children";
 import "./globals.css";
+
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "PolyLend",
@@ -18,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body className="min-h-screen flex flex-col">
         <Children>{children}</Children>
       </body>
