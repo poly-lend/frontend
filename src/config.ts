04fb31077c900e25d9a2ace837c1985ea7ae52ff
polylend-dev <polylend@tuta.com>
2025-12-11 21:22:11 +0400

    switch to usdc

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const usdcAddress = "0xf6b4Ae31f5C74191E920291c68e9769c4a46D3E4";
+export const usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
 export const usdcDecimals = 6;
 
 export const polylendDecimals = 18;
