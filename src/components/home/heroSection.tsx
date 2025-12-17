2f523299271c25eca4e5927284c8c926d5bca4f4
polylend-dev <polylend@tuta.com>
2025-12-17 08:08:33 +0400

    add missing file

--- src/components/home/heroSection.tsx
+++ src/components/home/heroSection.tsx
@@ -40,7 +40,7 @@ const HeroSection = () => {
         </p>
 
         <div className="mt-5 flex flex-wrap items-center gap-3">
-          <Link href="/borrow">
+          <Link href="/borrower-positions">
             <Button>Borrow now</Button>
           </Link>
           <Link href="https://docs.polylend.com" target="_blank">
