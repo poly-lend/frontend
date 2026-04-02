8c4ea20fd99e239c195ae835ea471184cc57f5ab
polylend-dev <polylend@tuta.com>
2026-04-02 05:07:11 +0200

    Fix hero Go to App button link to point to app.polylend.com

--- landing/src/components/home/heroSection.tsx
+++ landing/src/components/home/heroSection.tsx
@@ -40,7 +40,7 @@ const HeroSection = () => {
         </p>
 
         <div className="mt-5 flex flex-wrap items-center gap-3">
-          <Link href="/app">
+          <Link href="https://app.polylend.com">
             <Button>Go to App</Button>
           </Link>
           <Link href="https://docs.polylend.com" target="_blank">
